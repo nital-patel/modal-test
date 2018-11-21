7fab6cdae4162ee3baaf7d077090d2f0a3e5fa80
Nital Patel <patelnital36@gmail.com>
2018-11-21 12:31:58 -0500

    Add boxdetails

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ModalConductor from './components/ModalConductor';
 import SignInModal from './components/SignInModal';
 import ModalWrapper from './components/ModalWrapper';
+import BoxDetailsModal from './components/BoxDetailsModal';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <ModalConductor/>
           <SignInModal/>
           <ModalWrapper/>
+          <BoxDetailsModal/>
           </div>
 
       </div>
--- src/components/ModalConductor.jsx
+++ src/components/ModalConductor.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-import ExportDataModal from './ExportDataModal.jsx';
+import * as actions from '../../data/actions.js';
 import SignInModal from './SignInModal.jsx';
-import FeedbackModal from './FeedbackModal.jsx';
 import BoxDetailsModal from './BoxDetailsModal.jsx';
 
 const ModalConductor = props => {
     switch (props.currentModal) {
-        case 'EXPORT_DATA':
-            return <ExportDataModal {...props}/>;
-
         case 'SOCIAL_SIGN_IN':
             return <SignInModal {...props}/>;
-
-        case 'FEEDBACK':
-            return <FeedbackModal {...props}/>;
-
         case 'EDIT_BOX':
             return <BoxDetailsModal {...props}/>;
 
@@ -24,4 +17,4 @@ const ModalConductor = props => {
     }
 };
 
-export default ModalConductor;
+export default connect(state => state, () => actions)(ModalConductor);
